de73695e6eaec7ab493c980293b8ed6af8231491
ougrid <ougridd@gmail.com>
2023-12-05 23:50:08 +0700

    Add canvas functionality for drawing rectangles and lines

--- my_streamlit_app/app/main.py
+++ my_streamlit_app/app/main.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import cv2
 import numpy as np
 import PIL
+from streamlit_drawable_canvas import st_canvas
 
 
 def load_ceiling_type_model_and_labels():
@@ -29,6 +30,16 @@ def make_ceiling_type_prediction(model, image, class_names):
 
 
 def main():
+    canvas_config = {
+        "stroke_width": 3,
+        "stroke_color": "#FF0000",
+        "drawing_mode": "rect",
+        # "line_color": "#00FF00",
+        # "line_width": 3,
+        "background_image": None,
+    }
+    damage_result = 50
+
     model, class_names = load_ceiling_type_model_and_labels()
 
     st.title(":robot_face: TIP AI-assisted Claims")
@@ -73,10 +84,32 @@ def main():
     uploaded_file_damaged_01 = st.file_uploader(
         "Choose an image...", type=["jpg", "jpeg", "png"], key="damaged_01"
     )
-    damage_result = 50
     if uploaded_file_damaged_01 is not None:
         img_damaged_01 = uploaded_file_damaged_01.read()
         st.image(img_damaged_01)
+
+    if uploaded_file_damaged_01 is not None:
+        canvas_config["background_image"] = PIL.Image.open(uploaded_file_damaged_01)
+
+        # Allow users to choose between drawing a rectangle and a line
+        draw_mode = st.radio("Drawing tool:", ("Rectangle", "Line"))
+
+        if draw_mode == "Rectangle":
+            canvas_config["drawing_mode"] = "rect"
+            canvas_config["stroke_color"] = "#FF0000"  # Red for rectangle
+        else:
+            canvas_config["drawing_mode"] = "line"
+            canvas_config["stroke_color"] = "#00FF00"  # Green for line
+
+        # Create the canvas
+        canvas_result = st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)", key="canvas", **canvas_config
+        )
+        # Process the canvas data
+        if canvas_result.json_data is not None:
+            # Extract rectangle and line data from canvas_result.json_data here
+            pass
+
     ref_width = st.number_input(
         "Reference Object Width in Image No. 1 (cm)",
         value=0,
